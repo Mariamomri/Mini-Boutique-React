55c7e67e72b9ca12caf279407c6b6a671123cf70
Mariam <codegirlbxl@gmail.com>
2026-03-15 23:16:38 +0100

    add component SocialIcons & incons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import logo from '../assets/Logo montre minimali.png'
+import SocialIcons from '../components/SocialIcons'
 function Footer() { 
     return ( 
       <footer>
         <img src={logo} alt="logo" className="logo"/> 
+
+        
+        <SocialIcons/>
+
         <p >©Cfitech - Mariam Omri</p>
       </footer>
     ); 
